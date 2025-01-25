3e11387478136bbd6a3d6b1fd64fc20a783e1647
Andrew Bauer <zonexpertconsulting@outlook.com>
2025-01-25 12:05:14 -0600

    deprecate fedora 38 & 39, add fedora 40 & 41

--- index.php
+++ index.php
@@ -28,8 +28,8 @@ $releases = array();
 # This array is all you need to change, as new releases are added and old releases are deprecated.
 $releases[] = 8;
 $releases[] = 9;
-$releases[] = 38;
-$releases[] = 39;
+$releases[] = 40;
+$releases[] = 41;
 
 $zmrepo_rpms = array();
 $zmrepo_rpm = array();
